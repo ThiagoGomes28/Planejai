f3c6d25682b15cda41ab4b2b67ee3d104e9ea6b6
Thiago Gomes <thiago.g.sousa100@gmail.com>
2026-08-22 18:00:44 -0300

    Add: tailwindcss e Fonte Inter

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [react(), tailwindcss(), babel({ presets: [reactCompilerPreset()] })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
