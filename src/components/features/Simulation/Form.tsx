e0e8d52eead4e617c11dbd0ecdce50cbae85be11
Thiago Gomes <thiago.g.sousa100@gmail.com>
2026-08-25 08:44:38 -0300

    Add: Adicionar Interface do formulario

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,9 +1,21 @@
+import { PiggyBank } from 'lucide-react'
+import { FormStep } from './FormStep'
 import { StepProgress } from './Progress'
 
 export function SimulationForm() {
   return (
     <>
       <StepProgress currentStep={3} totalSteps={6} />
+      <FormStep
+        icon={PiggyBank}
+        title="Renda Mensal Bruta"
+        question="Quanto é depositado na sua conta todo mês? (Somando todos as fontes de renda)?"
+        inputProps={{
+          type: 'text',
+          placeholder: 'ex: 5.000,00',
+          prefix: 'R$'
+        }}
+      />
     </>
   )
 }
